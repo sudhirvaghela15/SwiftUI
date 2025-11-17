3cbf1f3ee651982dc01498086bc2744a7ea5fd28
sudhir <sudhirvaghela15@hotmail.com>
2025-11-18 00:44:05 +0530

    session one done added card view with isFaceup bool

--- Memorize/Memorize/Modules/Helper View's/ContentView.swift
+++ Memorize/Memorize/Modules/Helper View's/ContentView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-		ZStack {
-			RoundedRectangle(cornerRadius: 20)
-				.stroke(lineWidth: 2)
-				.overlay {
-					Text("☠️")
-						.font(.largeTitle)
-						.fontWeight(.bold)
-				}
+		HStack {
+			CardView(isFaceUp: true)
+			CardView()
+			CardView()
+			CardView()
 		}
-		.padding(.horizontal)
-		.foregroundStyle(.red)
+		.foregroundStyle(.orange)
+		.padding()
+		
     }
+}
+
+struct CardView: View {
+	var isFaceUp: Bool = false
+	
+	var body: some View {
+		ZStack {
+			if isFaceUp {
+				RoundedRectangle(cornerRadius: 12)
+					.foregroundStyle(.white)
+				
+				RoundedRectangle(cornerRadius: 12)
+					.strokeBorder(lineWidth: 2)
+				Text("☠️")
+					.font(.largeTitle)
+					.fontWeight(.bold)
+			} else {
+				RoundedRectangle(cornerRadius: 12)
+			}
+		}
+	}
 }
 
 #Preview {
