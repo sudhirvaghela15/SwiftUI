c97ce8ca804970a82355b2ea8a041b233486c680
sudhir <sudhirvaghela15@hotmail.com>
2025-12-01 00:36:55 +0530

    separated constants from code and crated enum also set proper transition for deck cards

--- Memorize/Memorize/Modules/Game/Views/EmojiMemoryGameView.swift
+++ Memorize/Memorize/Modules/Game/Views/EmojiMemoryGameView.swift
@@ -8,31 +8,56 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
+	
 	typealias Card = MemoryGame<String>.Card
-//	@State var viewModel: EmojiMemoryGame = .init()
+	
+	@Namespace private var dealingNamespace
+	
 	@Bindable var emojiMemoryGame: EmojiMemoryGame
+	
 	@State private var lastScoreChange = (0, causedByID: "")
-	private let aspectRatio: CGFloat = 2/3
-	private let spacing: CGFloat = 4
+	@State private var  delt =  Set<Card.ID>()
 	
-    var body: some View {
-	
+	var body: some View {
+		
 		VStack {
-			cards
-				.foregroundStyle(emojiMemoryGame.color)
+			cards.foregroundStyle(emojiMemoryGame.color)
 			bottomBar
 		}
 		.padding()
 		
-    }
+	}
+	
+	// MARK: - Custom view's
+	private var cards: some View {
+		AspectVGrid(
+			items: emojiMemoryGame.cards,
+			aspectRatio: Constant.aspectRatio
+		) { card in
+			if isDelt(card) {
+				CardView(card)
+					.matchedGeometryEffect(id: card.id, in: dealingNamespace)
+					.transition(.asymmetric(insertion: .identity, removal: .identity))
+					.padding(Constant.spacing)
+					.overlay(FlayingNumber(number: scoreChange(causeBy: card)))
+					.zIndex(cprint(scoreChange(causeBy: card) != 0 ? 100 : 0))
+					.onTapGesture { _ in
+						choose(card)
+					}
+			}
+		}
+	}
 	
 	private var bottomBar: some View {
 		HStack {
 			scoreView
 			Spacer()
+			deck.foregroundStyle(emojiMemoryGame.color)
+			Spacer()
 			shuffleView
 		}.font(.largeTitle)
 	}
+	
 	private var scoreView: some View {
 		Text("Score: \(emojiMemoryGame.score)")
 			.animation(nil)
@@ -48,42 +73,39 @@ struct EmojiMemoryGameView: View {
 		})
 	}
 	
-	private var cards: some View {
-		AspectVGrid(
-			items: emojiMemoryGame.cards,
-			aspectRatio: aspectRatio
-		) { card in
-			if isDelt(card) {
-				CardView(card)
-					   .padding(spacing)
-					   .overlay(FlayingNumber(number: scoreChange(causeBy: card)))
-					   .zIndex(cprint(scoreChange(causeBy: card) != 0 ? 100 : 0))
-					   .onTapGesture { _ in
-						   choose(card)
-					   }
-					   .transition(
-						.offset(
-							x: CGFloat.random(in: -1000...1000),
-							y: CGFloat.random(in: -1000...1000))
-					   )
+	private var deck: some View {
+		ZStack {
+			ForEach(undealtCards) {
+				CardView($0)
+					.matchedGeometryEffect(id: $0.id, in: dealingNamespace)
+					.transition(.asymmetric(insertion: .identity, removal: .identity))
 			}
-		}.onAppear {
-			withAnimation(.easeInOut(duration: 2)) {
-				for card in emojiMemoryGame.cards {
-					delt.insert(card.id)
-				}
+		}
+		.frame(width: Constant.deckWidth, height: Constant.deckHeight)
+		.onTapGesture {
+			deal()
+		}
+	}
+}
+
+	// MARK: - HELPER'S
+extension EmojiMemoryGameView {
+	private var undealtCards: [Card] {
+		emojiMemoryGame.cards.filter({ !isDelt($0) })
+	}
+	
+	private func deal() {
+		var delay: TimeInterval = 0
+		for card in emojiMemoryGame.cards {
+			withAnimation(Constant.dealAnimation.delay(delay)) {
+				_ = delt.insert(card.id)
 			}
+			delay += Constant.dealInterval
 		}
 	}
 	
-	@State private var  delt =  Set<Card.ID>()
-	
 	private func isDelt(_ card: Card) -> Bool {
 		delt.contains(card.id)
-	}
-	
-	private var undealtCards: [Card] {
-		emojiMemoryGame.cards.filter({ !isDelt($0) })
 	}
 	
 	private func choose(_ card: Card) {
@@ -98,6 +120,18 @@ struct EmojiMemoryGameView: View {
 	private func scoreChange(causeBy card: Card) -> Int {
 		let (amount, id) = lastScoreChange
 		return card.id == id ? amount : 0
+	}
+}
+
+	// MARK: - Constants
+private extension EmojiMemoryGameView {
+	enum Constant {
+		static let aspectRatio: CGFloat = 2/3
+		static let deckWidth: CGFloat = 50
+		static let deckHeight: CGFloat = deckWidth / aspectRatio
+		static let spacing: CGFloat = 4
+		static let dealAnimation = Animation.easeInOut(duration: 1)
+		static let dealInterval: TimeInterval = 0.15
 	}
 }
 
