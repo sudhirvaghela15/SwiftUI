621932b3677f0935d503ae994c27efab3dc90679
sudhir <sudhirvaghela15@hotmail.com>
2025-12-01 01:36:21 +0530

    added new project EmojiArt

---
+++ EmojiArt/EmojiArt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by sudhir on 01/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by sudhir on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
